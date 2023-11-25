9a96d8143ec29c7a549ca8c19289c885c4a360b0
KimSungEun226 <tjdt11@naver.com>
2023-11-25 18:37:47 +0900

    스프링 컨테이너 없는 수동 DI를 통한 테스트

--- springbook/user/test/UserDaoTest.java
+++ springbook/user/test/UserDaoTest.java
@@ -3,35 +3,26 @@ package springbook.user.test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.dao.UserDao;
 import springbook.user.domain.User;
 
-@RunWith(SpringJUnit4ClassRunner.class) //스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
-@ContextConfiguration(locations = "/applicationContext.xml") // 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정
+//@RunWith(SpringJUnit4ClassRunner.class) //스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
+//@ContextConfiguration(locations = "/test-applicationContext.xml") // 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정
 // 테스트 메소드에서 애플리케이션 컨텍스트의 구성이나 상태를 변경한다는 것을 테스트 컴텍스트 프레임워크에 알려준다.
 // 이 어노테이션이 붙은 테스트 클래스에는 애플리케이션 컨텍스트 공유를 허용하지 않는다.
-@DirtiesContext 
+// @DirtiesContext 
 public class UserDaoTest {
 	
 	
-	@Autowired
 	private UserDao dao;
 	private User user1;
 	private User user2;
@@ -40,6 +31,7 @@ public class UserDaoTest {
 	@Before
 	public void setUp() {
 		
+		dao = new UserDao();
 		DataSource dataSource = new SingleConnectionDataSource(
 				"jdbc:mysql://localhost:3306/tobi-spring", "tobi", "1234", true);
 		dao.setDataSource(dataSource);
