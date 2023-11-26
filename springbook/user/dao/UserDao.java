4ef4091f8870ac5cc870633f02dedff15deb6e9c
KimSungEun226 <tjdt11@naver.com>
2023-11-26 13:49:12 +0900

    예외처리 분리를 위한 전략패턴 적용

--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -19,17 +19,7 @@ public class UserDao {
 	}
 	
 	public void add(User user) throws SQLException {
-		Connection c = dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement(
-				"insert into users(id, name, password) values(?, ?, ?)");
-		ps.setString(1, user.getId());
-		ps.setString(2, user.getName());
-		ps.setString(3, user.getPassword());
-		
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
+		jdbcContextWithStatementStrategy(new AddStatement(user));
 	}
 	
 	public User get(String id) throws SQLException {
@@ -59,29 +49,75 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws SQLException {
-		Connection c = dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement(
-				"delete from users");
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
+		jdbcContextWithStatementStrategy(new DeleteAllStatement());
 	}
 	
 	public int getCount() throws SQLException {
-		Connection c = dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement(
-				"select count(*) from users");
 		
-		ResultSet rs = ps.executeQuery();
-		rs.next();
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
 		
-		int count = rs.getInt(1);
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement(
+					"select count(*) from users");
+			
+			rs = ps.executeQuery();
+			rs.next();
+			
+			int count = rs.getInt(1);
+			return count;
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				}catch (SQLException e) {}
+			}
+			
+			if (ps != null) {
+				try {
+					ps.close();
+				}catch (SQLException e) {}
+			}
+			
+			if (c != null) {
+				try {
+					c.close();
+				}catch (SQLException e) {}
+			}
+			
+		}
+	}
+	
+	public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+		Connection c = null;
+		PreparedStatement ps = null;
 		
-		rs.close();
-		ps.close();
-		c.close();
+		try {
+			c = dataSource.getConnection();
+			ps = stmt.makePreparedStatement(c);
+			ps.executeUpdate();
+		}catch (SQLException e) {
+			throw e;
+		} finally {
+			if(ps != null) {
+				try {
+					ps.close();
+				}catch (SQLException e) {
+					
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				}catch (SQLException e) {
+					
+				}
+			}			 
+		}
 		
-		return count;
 	}
 }
