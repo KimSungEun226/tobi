e2ecbcb4f685b0ac8185082385d6b35384d53051
KimSungEun226 <tjdt11@naver.com>
2023-11-28 23:12:30 +0900

    dao 추상화

--- springbook/user/dao/UserDao.java
+++ springbook/user/dao/UserDao.java
@@ -1,77 +1,17 @@
 package springbook.user.dao;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
-
-import javax.sql.DataSource;
-
-import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
-import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
-public class UserDao {
+public interface UserDao {
 	
-	private JdbcTemplate jdbcTemplate;
-	private RowMapper<User> userMapper = new RowMapper<User>() {
-		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-			return new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
-		}
-	};
-		
+	void add(User user);
 	
-	public void setDataSource(DataSource dataSource) {
-		//한 오브젝트의 수정자 메소드에서 다른 오브젝트를 초기화하고 코드를 이용해 DI하는 것은 스프링에서도 종종 사용되는 기법이다.
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
+	User get(String id);
 	
+	void deleteAll();
 	
-	public void add(final User user) throws SQLException {
-		
-		//콜백 오브젝트(StatementStragy)를 구현한 내부 클래스가 클라이언트 메소드 내의 정보(user)를 직접 참조하는것도 템플릿/콜백의 고유한 특징
-		this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
-				user.getId(), user.getName(), user.getPassword());
-	}
-	
-	public User get(String id) throws SQLException {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?", 
-			new Object[] {id}, // SQL에 바인딩할 파라미터 값, 가변인자 대신 배열을 사용
-			userMapper);
-	}
-	
-	public void deleteAll() throws SQLException {
-//		this.jdbcTemplate.update(
-//				new PreparedStatementCreator() {
-//					public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-//						// TODO Auto-generated method stub
-//						return con.prepareStatement("delete from users");
-//					}
-//				}
-//			);
-		this.jdbcTemplate.update("delete from users");
-	}
-	
-	public int getCount() throws SQLException {
-		return this.jdbcTemplate.query(new PreparedStatementCreator() {
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				return con.prepareStatement("select count(*) from users");
-			}
-		}, new ResultSetExtractor<Integer>() {
-			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException{
-				rs.next();
-				return rs.getInt(1);
-			}
-		});
-		//return this.jdbcTemplate.queryForInt("select count(*) from users");
-	}
+	int getCount();
 
-
-	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", userMapper);
-	}
+	List<User> getAll();
 }
--- springbook/user/test/UserDaoTest.java
+++ springbook/user/test/UserDaoTest.java
@@ -6,15 +6,21 @@ import static org.junit.Assert.assertThat;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import springbook.user.dao.UserDao;
+import springbook.user.dao.UserDaoJdbc;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class) //스프링의 테스트 컨텍스트 프레임워크의 JUnit 확장기능 지정
@@ -27,6 +33,8 @@ public class UserDaoTest {
 	
 	@Autowired
 	private UserDao dao;
+	@Autowired
+	DataSource dataSource;
 	private User user1;
 	private User user2;
 	private User user3;
@@ -119,5 +127,28 @@ public class UserDaoTest {
 		assertThat(user1.getId(), is(user2.getId()));
 		assertThat(user1.getName(), is(user2.getName()));
 		assertThat(user1.getPassword(), is(user2.getPassword()));
+	}
+	
+	@Test(expected = DuplicateKeyException.class)
+	public void duplicateKey() {
+		dao.deleteAll();
+		
+		dao.add(user1);
+		dao.add(user1);
+	}
+	
+	@Test
+	public void sqlExceptionTranslate() {
+		dao.deleteAll();
+		
+		try {
+			dao.add(user1);
+			dao.add(user1);
+		}catch (DuplicateKeyException ex) {
+			SQLException sqlEx = (SQLException)ex.getRootCause();
+			SQLExceptionTranslator set = 
+					new SQLErrorCodeSQLExceptionTranslator(this.dataSource);
+			assertThat(set.translate(null, null, sqlEx), is(DuplicateKeyException.class));
+		}
 	}
 }
