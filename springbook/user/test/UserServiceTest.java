b356b2eabe032acae584f815be176e9a33bbb998
KimSungEun226 <tjdt11@naver.com>
2023-12-04 22:09:44 +0900

    고립된 테스트를 위한 UserDao 목오브젝트 적용

--- springbook/user/test/UserServiceTest.java
+++ springbook/user/test/UserServiceTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.fail;
 import static springbook.user.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserServiceImpl.MIN_RECCOMEND_FOR_GOLD;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -63,19 +64,22 @@ public class UserServiceTest {
 	
 	@Test
 	public void upgradeLevels() throws Exception{
-		userDao.deleteAll();
-		for(User user : users) userDao.add(user);
+		UserServiceImpl userServiceImpl = new UserServiceImpl();
+		
+		MockUserDao mockUserDao = new MockUserDao(this.users);
+		userServiceImpl.setUserDao(mockUserDao);
+		
 		
 		MockMailSender mockMailSender = new MockMailSender();
 		userServiceImpl.setMailSender(mockMailSender);
 		
-		userService.upgradeLevels();
+		userServiceImpl.upgradeLevels();
 		
-		checkLevelUpgraded(users.get(0), false);
-		checkLevelUpgraded(users.get(1), true);
-		checkLevelUpgraded(users.get(2), false);
-		checkLevelUpgraded(users.get(3), true);
-		checkLevelUpgraded(users.get(4), false);
+		List<User> updated = mockUserDao.getUpdated();
+		assertThat(updated.size(), is(2));
+		
+		checkUserAndLevel(updated.get(0), "aaa2", Level.SILVER);
+		checkUserAndLevel(updated.get(1), "bbb4", Level.GOLD);
 		
 		List<String> requests = mockMailSender.getRequests();
 		assertThat(requests.size(), is(2));
@@ -128,6 +132,11 @@ public class UserServiceTest {
 		else assertThat(userUpdate.getLevel(), is(user.getLevel()));
 	}
 	
+	private void checkUserAndLevel(User updated, String expectedId, Level expectedLevel) {
+		assertThat(updated.getId(), is(expectedId));
+		assertThat(updated.getLevel(), is(expectedLevel));
+	}
+	
 	static class TestUserService extends UserServiceImpl {
 		private String id;
 		
@@ -138,6 +147,42 @@ public class UserServiceTest {
 		protected void upgradeLevel(User user) {
 			if (user.getId().equals(this.id)) throw new TestUserServiceException();
 			super.upgradeLevel(user);
+		}
+	}
+	
+	static class MockUserDao implements UserDao {
+		
+		private List<User> users; //레벨 업그레이드 후보 User 오브젝트 목록
+		private List<User> updated = new ArrayList<User>(); // 업그레이드 대상 오브젝트를 저장해둘 목록
+		
+		private MockUserDao(List<User> users) {
+			this.users = users;
+		}
+		
+		public List<User> getUpdated() {
+			return this.updated;
+		}
+		
+		@Override
+		public void add(User user) { throw new UnsupportedOperationException(); }
+
+		@Override
+		public User get(String id) { throw new UnsupportedOperationException(); }
+
+		@Override
+		public void deleteAll() { throw new UnsupportedOperationException(); }
+
+		@Override
+		public int getCount() { throw new UnsupportedOperationException(); }
+
+		@Override
+		public List<User> getAll() {
+			return this.users; //스텁기능제공
+		}
+		
+		@Override
+		public void update(User user) {
+			updated.add(user);  //목 오브젝트 기능 제공
 		}
 	}
 	
