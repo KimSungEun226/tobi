0b16071086a4ac4eb57b2143b6f75fb24b2bd6ee
KimSungEun226 <tjdt11@naver.com>
2023-12-11 22:57:28 +0900

    스프링 트랜잭션 경계설정 어드바이스 TransactionInterceptor 적용

--- springbook/user/test/UserServiceTest.java
+++ springbook/user/test/UserServiceTest.java
@@ -22,6 +22,7 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.dao.TransientDataAccessResourceException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.test.annotation.DirtiesContext;
@@ -163,6 +164,11 @@ public class UserServiceTest {
 		assertThat(testUserService, is(java.lang.reflect.Proxy.class));
 	}
 	
+	@Test(expected=TransientDataAccessResourceException.class)
+	public void readOnlyTransactionAttribute() {
+		testUserService.getAll();
+	}
+	
 	private void checkLevelUpgraded(User user, boolean upgraded) {
 		User userUpdate = userDao.get(user.getId());
 		if (upgraded) assertThat(userUpdate.getLevel(), is(user.getLevel().nextLevel()));
@@ -185,6 +191,13 @@ public class UserServiceTest {
 		protected void upgradeLevel(User user) {
 			if (user.getId().equals(this.id)) throw new TestUserServiceException();
 			super.upgradeLevel(user);
+		}
+		
+		public List<User> getAll() {
+			for(User user : super.getAll()) {
+				super.update(user);
+			}
+			return null;
 		}
 	}
 	
