234ba80590f71506373b701d73e229bb3fba657c
KimSungEun226 <tjdt11@naver.com>
2023-12-31 16:35:30 +0900

    SQL 서비스 인터페이스

--- springbook/user/dao/UserDaoJdbc.java
+++ springbook/user/dao/UserDaoJdbc.java
@@ -1,20 +1,16 @@
 package springbook.user.dao;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
+import springbook.user.sqlservice.SqlService;
 
 public class UserDaoJdbc implements UserDao{
 	
@@ -30,6 +26,8 @@ public class UserDaoJdbc implements UserDao{
 					rs.getString("email"));
 		}
 	};
+	
+	private SqlService sqlService;
 		
 	
 	public void setDataSource(DataSource dataSource) {
@@ -37,17 +35,20 @@ public class UserDaoJdbc implements UserDao{
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
+	public void setSqlService(SqlService sqlService) {
+		this.sqlService = sqlService;
+	}
 	
 	public void add(User user) {
 		
 		//콜백 오브젝트(StatementStragy)를 구현한 내부 클래스가 클라이언트 메소드 내의 정보(user)를 직접 참조하는것도 템플릿/콜백의 고유한 특징
-		this.jdbcTemplate.update("insert into users(id, name, password, level, login, recommend, email) values(?, ?, ?, ?, ?, ?, ?)",
+		this.jdbcTemplate.update(this.sqlService.getSql("userAdd"),
 				user.getId(), user.getName(), user.getPassword(), 
 				user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail());
 	}
 	
 	public User get(String id) {
-		return this.jdbcTemplate.queryForObject("select * from users where id = ?", 
+		return this.jdbcTemplate.queryForObject(this.sqlService.getSql("userGet"), 
 			new Object[] {id}, // SQL에 바인딩할 파라미터 값, 가변인자 대신 배열을 사용
 			userMapper);
 	}
@@ -61,34 +62,33 @@ public class UserDaoJdbc implements UserDao{
 //					}
 //				}
 //			);
-		this.jdbcTemplate.update("delete from users");
+		this.jdbcTemplate.update(this.sqlService.getSql("userDeleteAll"));
 	}
 	
 	public int getCount() {
-		return this.jdbcTemplate.query(new PreparedStatementCreator() {
-			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-				return con.prepareStatement("select count(*) from users");
-			}
-		}, new ResultSetExtractor<Integer>() {
-			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException{
-				rs.next();
-				return rs.getInt(1);
-			}
-		});
-		//return this.jdbcTemplate.queryForInt("select count(*) from users");
+//		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+//				return con.prepareStatement("select count(*) from users");
+//			}
+//		}, new ResultSetExtractor<Integer>() {
+//			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException{
+//				rs.next();
+//				return rs.getInt(1);
+//			}
+//		});
+		return this.jdbcTemplate.queryForInt(this.sqlService.getSql("userGetCount"));
 	}
 
 
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id", userMapper);
+		return this.jdbcTemplate.query(this.sqlService.getSql("userGetAll"), userMapper);
 	}
 
 
 	@Override
 	public void update(User user) {
 		this.jdbcTemplate.update(
-			"update users set name = ?, password = ?, level = ?, login =?, " + 
-			"recommend = ?, email = ? where id = ?", user.getName(), user.getPassword(),
+				this.sqlService.getSql("userUpdate"), user.getName(), user.getPassword(),
 			user.getLevel().intValue(), user.getLogin(), user.getRecommend(), user.getEmail(), user.getId());
 	}
 }
